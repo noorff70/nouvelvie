2fa2643cef46c621ff086d1c10d0ab55f03bf6aa
noorf <faizul.noor@toyota.com>
2021-12-31 23:31:08 -0500

    Firstcommit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ClientAuthorizationComponent } from './components/client-authorization/client-authorization.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  /* default */
+  { path: '', redirectTo: '/nouvelvie', pathMatch: 'full' },
+  {path: 'nouvelvie', component: ClientAuthorizationComponent},
+
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClientAuthorizationComponent } from './components/client-authorization/client-authorization.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientAuthorizationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
